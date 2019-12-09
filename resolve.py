0658f96bf7adabfb307e86e4e83dbfcc386b7ca0
Kandyce Burks <kandyce.burks@relativity.com>
2019-12-09 15:56:35 -0600

    [develop] added user agent.

--- resolve.py
+++ resolve.py
@@ -43,7 +43,11 @@ def ResolveURL(url):
 
     ImportModules()
 
-    response = urllib2.urlopen(url)
+
+    user_agent = {'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'}
+
+    request = urllib2.request(url, user_agent)
+    response = urllib2.urlopen(request)
     html = response.read()
     #html = utils.getHTML(url)
     #html = html.replace('"Click Here!!"</a></div>', '')
